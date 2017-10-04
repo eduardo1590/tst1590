708e07e437503f9c3a795c5180f22a74707d8d2c
eduardo1590 <eduardo1590@gmail.com>
2017-10-04 19:35:55 -0400

    footer responsivo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { TotemCorporativosPage } from '../totem-corporativos/totem-corporativos';
 import { TotemEventosPage } from '../totem-eventos/totem-eventos';
+import { ConfiguracionPage } from '../configuracion/configuracion';
 
 @Component({
   selector: 'page-home',
@@ -19,6 +20,10 @@ export class HomePage {
 
   goToCorporativos(){
     this.navCtrl.push(TotemCorporativosPage);
+  }
+
+  goToConfiguracion(){
+    this.navCtrl.push(ConfiguracionPage);
   }
 
 }
