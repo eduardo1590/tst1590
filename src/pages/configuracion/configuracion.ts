0dd7fc577a5301586d3206daf11aaf2413ef5a70
eduardo1590 <eduardo1590@gmail.com>
2017-10-04 22:07:05 -0400

    nombre y logo de evento

--- src/pages/configuracion/configuracion.ts
+++ src/pages/configuracion/configuracion.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 /**
  * Generated class for the ConfiguracionPage page.
@@ -14,11 +15,29 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ConfiguracionPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  nombreEvento: string = '';
+  logoEvento: any = '';
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public camera: Camera) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ConfiguracionPage');
+  }
+
+  seleccionarLogo(){
+    let options = {
+      destinationType   : this.camera.DestinationType.DATA_URL,
+      sourceType        : this.camera.PictureSourceType.PHOTOLIBRARY
+    };
+
+    this.camera.getPicture(options)
+    .then(data =>{
+      this.logoEvento = "data:image/jpeg;base64," + data;
+    })
+    .catch(error =>{
+      console.error( error );
+    });
   }
 
 }
